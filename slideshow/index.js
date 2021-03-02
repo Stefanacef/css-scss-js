ae3ebdca06820954e525ac2f53078cab39d0f8b4
Stefana <farcasus@yahoo.com>
2021-03-02 17:12:01 +0200

    Create index.js

---
+++ slideshow/index.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+   
+    <div class="container">
+     <div class="panel  panel__1 activ ">
+      <h3><strong>LearnnCSS </strong></h3>
+     </div>
+     <div class="panel panel__2">
+      <h3><strong>LearnnCSS</strong></h3>
+     </div>
+     <div class="panel  panel__3">
+       <h3><strong>LearnnCSS</strong></h3>
+     </div>
+     <div class="panel panel__4  ">
+       <h3><strong>LearnnCSS</strong></h3>
+     </div>
+     <div class="panel panel__5 ">
+      <h3><strong>LearnnCSS</strong></h3>
+     </div>
+  
+    </div>
+
+
+<script src="index.js"></script>
+</body>
+</html>
